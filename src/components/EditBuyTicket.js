cf89e3b3314f1c98aa070ee9e20b0fd4ddac6ace
SCARRAIDER <rsahanediriweera@gmail.com>
2023-02-16 17:25:34 +0530

    completed all the creation of components

---
+++ src/components/EditBuyTicket.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditBuyTicket = () => {
+  return (
+    <div>EditBuyTicket</div>
+  )
+}
+
+export default EditBuyTicket
---
+++ src/components/EditSellTicket.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditSellTicket = () => {
+  return (
+    <div>EditSellTicket</div>
+  )
+}
+
+export default EditSellTicket
