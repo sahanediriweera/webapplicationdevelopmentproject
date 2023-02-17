af92d9d8ef3b510e6fe5a68731b4db029753933d
SCARRAIDER <rsahanediriweera@gmail.com>
2023-02-17 21:23:29 +0530

    initial static completion

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -22,25 +22,27 @@ const SignUp = () => {
         <img src={Pic2} alt="backpic2" style={{ position: 'absolute', top: 0, left: '33.33%', height: '100%', width: '33.33%', objectFit: 'cover' }} />
         <img src={Pic3} alt="backpic3" style={{ position: 'absolute', top: 0, left: '66.66%', height: '100%', width: '33.33%', objectFit: 'cover' }} />
       </div>
+      <div className="form-container">
       <form className="form" onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" value={name} onChange={(event) => setName(event.target.value)} />
-      </div>
-      <div className="form-group">
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" value={email} onChange={(event) => setEmail(event.target.value)} />
-      </div>
-      <div className="form-group">
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)} />
-      </div>
-      <div className="form-group">
-        <label htmlFor="confirmPassword">Confirm Password:</label>
-        <input type="password" id="confirmPassword" value={confirmPassword} onChange={(event) => setConfirmPassword(event.target.value)} />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" value={name} onChange={(event) => setName(event.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" value={email} onChange={(event) => setEmail(event.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input type="password" id="confirmPassword" value={confirmPassword} onChange={(event) => setConfirmPassword(event.target.value)} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   </div>
   )
 }
