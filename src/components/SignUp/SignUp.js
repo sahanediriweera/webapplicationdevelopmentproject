ec5519f26598af184a12f4bd87911be585700ca6
SCARRAIDER <rsahanediriweera@gmail.com>
2023-02-17 20:56:33 +0530

    created initial signup page

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,8 +1,37 @@
 import React from 'react'
 
 const SignUp = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // submit form data to server or perform validation here
+  };
   return (
-    <div>SignUp</div>
+    <div>
+      <form className="form" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" value={name} onChange={(event) => setName(event.target.value)} />
+      </div>
+      <div className="form-group">
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" value={email} onChange={(event) => setEmail(event.target.value)} />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+      </div>
+      <div className="form-group">
+        <label htmlFor="confirmPassword">Confirm Password:</label>
+        <input type="password" id="confirmPassword" value={confirmPassword} onChange={(event) => setConfirmPassword(event.target.value)} />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
   )
 }
 
