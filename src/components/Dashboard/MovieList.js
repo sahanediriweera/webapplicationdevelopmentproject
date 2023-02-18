14d493ea3a124aea2202cb91038ff2982fb4f218
SCARRAIDER <rsahanediriweera@gmail.com>
2023-02-19 00:17:36 +0530

    completed get functiionalities in dashboard

--- src/components/Dashboard/MovieList.js
+++ src/components/Dashboard/MovieList.js
@@ -1,23 +1,56 @@
-import React from 'react';
+import axios from 'axios';
+import React,{useState} from 'react';
 import './MovieList.css';
 
 function MovieList() {
-  const englishMovies = [
+  const [englishMovies,setEnglishMovies] = useState([
     { name: 'Movie 1', purchases: 10 },
     { name: 'Movie 2', purchases: 20 },
     { name: 'Movie 3', purchases: 5 },
-  ];
-  const sinhalaMovies = [
+  ]);
+  const [sinhalaMovies,setSinhalaMovies] = useState([
     { name: 'සිංහල චිත්‍රපට 1', purchases: 15 },
     { name: 'සිංහල චිත්‍රපට 2', purchases: 8 },
     { name: 'සිංහල චිත්‍රපට 3', purchases: 12 },
-  ];
-  const tamilMovies = [
+  ]);
+  const [tamilMovies,setTamilMovies] = useState([
     { name: 'தமிழ் திரைப்படம் 1', purchases: 25 },
     { name: 'தமிழ் திரைப்படம் 2', purchases: 18 },
     { name: 'தமிழ் திரைப்படம் 3', purchases: 6 },
-  ];
+  ]);
 
+  
+  const getSinhalaMovieData = ()=> {
+    axios.get('')
+    .then(
+      response=> {
+        setSinhalaMovies(response.data);
+      }
+    ).catch(err=> {
+      console.log(err);
+    });
+  };
+  const getEnglishMovieData = ()=> {
+    axios.get('')
+    .then(
+      response=> {
+        setEnglishMovies(response.data);
+      }
+    ).catch(err=> {
+      console.log(err);
+    });
+  };
+  const getTamilMovieData = ()=> {
+    axios.get('')
+    .then(
+      response=> {
+        setTamilMovies(response.data);
+      }
+    ).catch(err=> {
+      console.log(err);
+    });
+  };
+  
   return (
     <div className="movie-list-container">
       <h2 className="movie-list-heading">Movie List</h2>
