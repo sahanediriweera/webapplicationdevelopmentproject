c3d867ac4f9ce99fbc5678f945a2458f8ac993fc
SCARRAIDER <rsahanediriweera@gmail.com>
2023-02-18 12:12:59 +0530

    static completion of edit buy ticket

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import EditBuyTicket from "./components/EditBuyTicket/EditBuyTicket";
 import Navigation from "./components/Navigation";
 //import Seller from "./components/SellingPage/Seller";
 //import SellTicket from "./components/SellTicket/SellTicket";
 //import SignUp from "./components/SignUp/SignUp";
 //import Dashboard from './components/Dashboard/Dashboard';
 //import Login from "./components/Login/Login";
-import BuyTicket from "./components/BuyTicket/BuyTicket";
+//import BuyTicket from "./components/BuyTicket/BuyTicket";
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
       {/* <SignUp/> */}
       {/* <Seller/> */}
       {/* <SellTicket/> */}
-      <BuyTicket/>
+      {/* <BuyTicket/> */}
+      <EditBuyTicket/>
     </div>
   );
 }
--- src/components/EditBuyTicket.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const EditBuyTicket = () => {
-  return (
-    <div>EditBuyTicket</div>
-  )
-}
-
-export default EditBuyTicket
