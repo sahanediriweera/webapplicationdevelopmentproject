cd9fd123f4cd8bf6498a6099bfb00c70e1dafd3a
SCARRAIDER <rsahanediriweera@gmail.com>
2023-02-23 09:12:25 +0530

    completed the use effects in the dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -25,6 +25,24 @@ const Dashboard = () => {
     { name: 'தமிழ் திரைப்படம் 3', purchases: 6 },
   ]);
 
+  const [movies,setMovies] = useState([
+    {
+      id: 1,
+      title: "The Matrix Resurrections",
+      description: "The fourth installment in The Matrix film franchise."
+    },
+    {
+      id: 2,
+      title: "Dune",
+      description: "A feature adaptation of Frank Herbert's science fiction novel, about the son of a noble family entrusted with the protection of the most valuable asset and most vital element in the galaxy."
+    },
+    {
+      id: 3,
+      title: "The French Dispatch",
+      description: "A love letter to journalists set in an outpost of an American newspaper in a fictional 20th-century French city that brings to life a collection of stories published in The French Dispatch Magazine."
+    }
+  ]);
+
   useEffect(()=>{
     axios.get('https://localhost:7035/api/Home/english')
     .then(response => {
@@ -47,8 +65,14 @@ const Dashboard = () => {
     .catch(error=> {
       console.log(error);
     });
+    axios.get('get upcoming movies')
+    .then(response => {
+        setMovies(response.data);
+    })
+    .catch(error=> {
+      console.log(error);
+    });
   },[]);
-
 
   return (
     <div>
@@ -65,7 +89,7 @@ const Dashboard = () => {
           <MovieSearch style={{flex:1, margin:'0 10px'}}/>
         </div>
         <div style={{width:'33%'}}>
-        <UpcomingMovies style={{flex:1, margin:'0 10px'}}/>
+        <UpcomingMovies movies={movies} style={{flex:1, margin:'0 10px'}}/>
         </div>
       </div>
     </div>
--- src/components/Dashboard/UpComingMovies.js
+++ src/components/Dashboard/UpComingMovies.js
@@ -2,25 +2,7 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import './UpComingMovies.css';
 
-const UpcomingMovies = () => {
-  const [movies,setMovies] = useState([
-    {
-      id: 1,
-      title: "The Matrix Resurrections",
-      description: "The fourth installment in The Matrix film franchise."
-    },
-    {
-      id: 2,
-      title: "Dune",
-      description: "A feature adaptation of Frank Herbert's science fiction novel, about the son of a noble family entrusted with the protection of the most valuable asset and most vital element in the galaxy."
-    },
-    {
-      id: 3,
-      title: "The French Dispatch",
-      description: "A love letter to journalists set in an outpost of an American newspaper in a fictional 20th-century French city that brings to life a collection of stories published in The French Dispatch Magazine."
-    }
-  ]);
-
+const UpcomingMovies = ({movies}) => {
 
   // const getData = () => {
   //   axios.get('https://localhost:7035/api/Home/movies')
