c99c3bb47813a166dbe22e76d8de64ffa5d54b43
SCARRAIDER <rsahanediriweera@gmail.com>
2023-02-26 11:52:17 +0530

    routing enabled

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-//import EditBuyTicket from "./components/EditBuyTicket/EditBuyTicket";
+import EditBuyTicket from "./components/EditBuyTicket/EditBuyTicket";
 import EditSellTicket from "./components/EditSellTicket/EditSellTicket";
 import Navigation from "./components/Navigation";
-//import Seller from "./components/SellingPage/Seller";
-//import SellTicket from "./components/SellTicket/SellTicket";
-//import SignUp from "./components/SignUp/SignUp";
-//import Dashboard from './components/Dashboard/Dashboard';
-//import Login from "./components/Login/Login";
-// import BuyTicket from "./components/BuyTicket/BuyTicket";
+import Seller from "./components/SellingPage/Seller";
+import SellTicket from "./components/SellTicket/SellTicket";
+import SignUp from "./components/SignUp/SignUp";
+import Dashboard from './components/Dashboard/Dashboard';
+import Login from "./components/Login/Login";
+import BuyTicket from "./components/BuyTicket/BuyTicket";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element:<Login/>
+    },
+    {
+      path:'/dashboard',
+      element:<Dashboard/>
+    },
+    {
+      path:'/editbuyticket',
+      element:<EditBuyTicket/>
+    },
+    {
+      path:'/editsellticket',
+      element:<EditSellTicket/>
+    },
+    {
+      path:'/sellticket',
+      element:<SellTicket/>
+    },
+    {
+      path:'/buyticket',
+      element:<BuyTicket/>
+    },
+    {
+      path:'/signup',
+      element:<SignUp/>
+    },
+    {
+      path:'/seller',
+      element:<Seller/>
+    },
+  ])
+
   return (
-    <div className="App">
-      <Navigation/>
-      {/* <Dashboard/> */}
-      {/* <Login/> */}
-      {/* <SignUp/> */}
-      {/* <Seller/> */}
-      {/* <SellTicket/> */}
-      {/* <BuyTicket/> */}
-      {/* <EditBuyTicket/> */}
-      <EditSellTicket/>
-    </div>
+    <RouterProvider router={router}/>
   );
 }
 
--- src/components/Dashboard/UpComingMovies.js
+++ src/components/Dashboard/UpComingMovies.js
@@ -2,39 +2,8 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import './UpComingMovies.css';
 
-const UpcomingMovies = () => {
+const UpcomingMovies = ({movies}) => {
 
-  const [movies,setMovies] = useState([
-    {
-      id: 1,
-      title: "The Matrix Resurrections",
-      description: "The fourth installment in The Matrix film franchise."
-    },
-    {
-      id: 2,
-      title: "Dune",
-      description: "A feature adaptation of Frank Herbert's science fiction novel, about the son of a noble family entrusted with the protection of the most valuable asset and most vital element in the galaxy."
-    },
-    {
-      id: 3,
-      title: "The French Dispatch",
-      description: "A love letter to journalists set in an outpost of an American newspaper in a fictional 20th-century French city that brings to life a collection of stories published in The French Dispatch Magazine."
-    }
-  ]);
-
-  useEffect(()=>{
-    axios.get('https://localhost:7035/api/Home/movies')
-    .then(response => {
-      setMovies(response.data);
-    })
-    .catch(error=> {
-      console.log(error);
-    });
-  })
-
-  // const getData = () => {
-
-  // };
 
   return (
     <div className="upcoming-movies">
